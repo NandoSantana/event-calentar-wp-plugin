eb24b02a15ec7182f61e1449368579190dd04c0e
Fernando PC <bh.carvalho@gmail.com>
2020-05-23 22:59:28 -0300

    review and remove last custom post types

--- src/Submenu.php
+++ src/Submenu.php
@@ -16,8 +16,7 @@ public $submenu_page;
   public function init() 
   {
 
-   // add_action( 'admin_menu', array( $this, 'scheduler_admin_menus' ) );
-    add_action( 'admin_menu', array( $this,'custom_post_calendar') );
+    add_action( 'admin_menu', array( $this, 'scheduler_admin_menus' ) );
  
   }
 
@@ -36,39 +35,6 @@ public $submenu_page;
     
     add_submenu_page('event-calendar', __('Edit Events', 'event-calendar'), __('Edit Events', 'event-calendar'), 'manage_options', 'edit-events',  array( $this->submenu_page, 'Edit' ) );
   }
-  public function custom_post_calendar() {
-    
-    // Set the labels, this variable is used in the $args array
-    $labels = array(
-        'name'               => __( 'Movie Reviews' ),
-        'singular_name'      => __( 'Movie Review' ),
-        'add_new'            => __( 'Add New Movie Review' ),
-        'add_new_item'       => __( 'Add New Movie Review' ),
-        'edit_item'          => __( 'Edit Movie Review' ),
-        'new_item'           => __( 'New Movie Review' ),
-        'all_items'          => __( 'All Movie Reviews' ),
-        'view_item'          => __( 'View Movie Reviews' ),
-        'search_items'       => __( 'Search Movie Reviews' )
-    );
-    
-    // The arguments for our post type, to be entered as parameter 2 of register_post_type()
-    $args = array(
-        'labels'            => $labels,
-        'description'       => 'Holds our movie reviews',
-        'public'            => true,
-        'menu_position'     => 6,
-        'supports'          => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments', 'custom-fields' ),
-        'has_archive'       => true,
-        'show_in_admin_bar' => true,
-        'show_in_nav_menus' => true,
-        'has_archive'       => true
-    );
-    
-    // Call the actual WordPress function
-    // Parameter 1 is a name for the post type
-    // $args array goes in parameter 2.
-    register_post_type( 'event-calendar', $args);
-    }
-
+ 
 
 }
--- src/SubmenuPage.php
+++ src/SubmenuPage.php
@@ -11,55 +11,13 @@ class SubmenuPage {
 */
     public function renderConfig() {
 
-        echo _e('This is the basic submenu page.', '');
+        echo _e('<h2>This is basic submenu page.</h2>', '');
     }
 
     public function Edit(){
 
-        echo "Edit";
+        echo "<h2>Second Menu</h2>";
        
     }
-    
-    // The custom function to register a movie post type
-    public function lc_custom_post_calendar() {
- 
-    // Set the labels, this variable is used in the $args array
-    $labels = array(
-      'name'               => __( 'Movies' ),
-      'singular_name'      => __( 'Movie' ),
-      'add_new'            => __( 'Add New Movie' ),
-      'add_new_item'       => __( 'Add New Movie' ),
-      'edit_item'          => __( 'Edit Movie' ),
-      'new_item'           => __( 'New Movie' ),
-      'all_items'          => __( 'All Movies' ),
-      'view_item'          => __( 'View Movie' ),
-      'search_items'       => __( 'Search Movies' ),
-      'featured_image'     => 'Poster',
-      'set_featured_image' => 'Add Poster'
-    );
-   
-    // The arguments for our post type, to be entered as parameter 2 of register_post_type()
-    $args = array(
-      'labels'            => $labels,
-      'description'       => 'Holds our movies and movie specific data',
-      'public'            => true,
-      'menu_position'     => 5,
-      'supports'          => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments', 'custom-fields' ),
-      'has_archive'       => true,
-      'show_in_admin_bar' => true,
-      'show_in_nav_menus' => true,
-      'has_archive'       => true,
-      'query_var'         => 'film'
-    );
-   
-    // Call the actual WordPress function
-    // Parameter 1 is a name for the post type
-    // Parameter 2 is the $args array
-    register_post_type('movie', $args);
-  }
-  
- 
-
- 
 
 }
